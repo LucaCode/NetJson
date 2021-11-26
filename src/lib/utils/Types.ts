f2a9a5b95d42402b464946f6f402fe80a112654e
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-11-26 13:35:29 +0100

    Create type utils.

---
+++ src/lib/utils/Types.ts
@@ -0,0 +1,9 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
+export type TypedArray = Uint8Array | Int8Array | Uint16Array |
+    Int16Array | Uint32Array | Int32Array | BigInt64Array |
+    BigUint64Array | Float32Array | Float64Array;
