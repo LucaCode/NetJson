89ab8ece46b6406c3fe5644035396836cc74e1f7
Luca Scaringella <32013416+LucaCode@users.noreply.github.com>
2021-11-26 13:35:15 +0100

    Create DataType ranges constants.

---
+++ src/lib/utils/DataTypeRanges.ts
@@ -0,0 +1,23 @@
+/*
+Author: Ing. Luca Gian Scaringella
+GitHub: LucaCode
+Copyright(c) Ing. Luca Gian Scaringella
+ */
+
+export const U_INT_8_MIN = 0;
+export const U_INT_8_MAX = 255;
+
+export const S_INT_8_MIN = -128;
+export const S_INT_8_MAX = 127;
+
+export const U_INT_16_MIN = 0;
+export const U_INT_16_MAX = 65535;
+
+export const S_INT_16_MIN = -32768;
+export const S_INT_16_MAX = 32767;
+
+export const U_INT_32_MIN = 0;
+export const U_INT_32_MAX = 4294967295;
+
+export const S_INT_32_MIN = -2147483648;
+export const S_INT_32_MAX = 2147483647;
